c6d007b6c162359443cde7399c3dcd16f3ba38a9
Anton Alekseev <undesazar7@gmail.com>
2025-04-14 12:18:15 +0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace ConsoleApp1
+{
+    internal class ProgramTest
+    {
+        static object locker = new object();
+        static async Task Main(string[] args)
+        {
+            var transferHepler = new TransferHelper();
+
+            var accountOne = new Account();
+            accountOne.Amount = 1000;
+
+            var accountTwo = new Account();
+            accountTwo.Amount = 250;
+
+            /*var tasks = new Task[100];
+
+            for (var i = 0; i < tasks.Length; i++)
+            {
+                tasks[i] = Task.Run(async() =>
+                {
+                    transferHepler.Transfer(accountOne, accountTwo, 10);
+                    Console.WriteLine($@"Balance of account A is {accountOne.Amount}, balance of account B is {accountTwo.Amount}");
+                });
+            }*/
+
+            const int threadsCount = 100;
+            var threads = new List<Thread>();
+
+            for (var i = 0; i < threadsCount; i++)
+            {
+                int threadId = i;
+                var thread = new Thread(() =>
+                {
+                    Thread.Sleep(1000);
+                    lock (locker)
+                    {
+                        transferHepler.Transfer(accountOne, accountTwo, 10);
+                        Console.WriteLine($@"Balance of account A is {accountOne.Amount}, balance of account B is {accountTwo.Amount}");
+                    }
+                });
+                thread.IsBackground = true;
+
+                threads.Add(thread);
+                
+                thread.IsBackground = true;
+            }
+
+            threads.ForEach(thread => thread.Start());
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApp1/TransferHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class TransferHelper
+    {
+        private readonly object _locker = new object();
+
+        public void Transfer(Account a, Account b, decimal amount)
+        {
+            lock (_locker)
+            {
+                a.Amount -= amount;
+                b.Amount += amount;
+                // Console.WriteLine($@"Balance of account A is {a.Amount}, balance of account B is {b.Amount}");
+            }
+        }
+    }
+}
