56b5c18c3ab687a5bc129630caf3173f9f173087
Anton Alekseev <undesazar7@gmail.com>
2025-04-14 14:27:10 +0300

    Added some check; removed comments

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -2,54 +2,32 @@
 {
     internal class ProgramTest
     {
-        // static object locker = new object();
         static async Task Main(string[] args)
         {
             var transferHepler = new TransferHelper();
 
             var accountOne = new Account(1000);
-            // accountOne.Amount = 1000;
-
             var accountTwo = new Account(250);
-            // accountTwo.Amount = 250;
-
-            /*var tasks = new Task[100];
-
-            for (var i = 0; i < tasks.Length; i++)
-            {
-                tasks[i] = Task.Run(async() =>
-                {
-                    transferHepler.Transfer(accountOne, accountTwo, 10);
-                    Console.WriteLine($@"Balance of account A is {accountOne.Amount}, balance of account B is {accountTwo.Amount}");
-                });
-            }*/
 
             const int threadsCount = 100;
             var threads = new List<Thread>();
 
             for (var i = 0; i < threadsCount; i++)
             {
-                int threadId = i;
                 var thread = new Thread(() =>
                 {
                     Thread.Sleep(1000);
-                    /*lock (locker)
-                    {
-                        
-                    }*/
-
                     transferHepler.Transfer(accountOne, accountTwo, 10);
-                    Console.WriteLine($@"Balance of account A is {accountOne.Amount}, balance of account B is {accountTwo.Amount}");
                 });
                 thread.IsBackground = true;
 
                 threads.Add(thread);
-                
-                thread.IsBackground = true;
             }
 
             threads.ForEach(thread => thread.Start());
-
+            threads.ForEach(thread => thread.Join());
+            Console.WriteLine($@"Balance of account A is {accountOne.Amount}, balance of account B is {accountTwo.Amount}");
+            Console.WriteLine($@"Balance of account A is 0: {accountOne.Amount == 0}, balance of account B is 1250: {accountTwo.Amount == 1250}");
             Console.ReadKey();
         }
     }
--- ConsoleApp1/TransferHelper.cs
+++ ConsoleApp1/TransferHelper.cs
@@ -8,8 +8,6 @@ namespace ConsoleApp1
 {
     public class TransferHelper
     {
-        // private readonly object _locker = new object();
-
         public void Transfer(Account a, Account b, decimal amount)
         {
             if (a.Id < b.Id)
@@ -33,15 +31,6 @@ namespace ConsoleApp1
                 Monitor.Exit(a.m_balanceLock);
                 Monitor.Exit(b.m_balanceLock);
             }
-
-
-            /*lock (_locker)
-            {
-                
-                 a.Amount -= amount;
-                 b.Amount += amount;
-                 // Console.WriteLine($@"Balance of account A is {a.Amount}, balance of account B is {b.Amount}");
-            }*/
         }
     }
 }
